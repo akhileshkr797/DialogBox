4c1afed9c6cef5bd5b44c0acf1604927baf20143
akhileshkr797 <akhileshkr797@gmail.com>
2019-05-06 10:55:42 +0530

    added dialogBox

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
 const electron = require('electron')
-const {app, BrowserWindow} = electron
+const { app, BrowserWindow, ipcMain, webContents, dialog } = electron
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
 
 let mainWindow
 
-function createWindow(){
+function createWindow() {
     mainWindow = new BrowserWindow({
-        show:false,
-        width:1350,
-        height:800,
-        title:'mainWindow',
-
+        show: false,
+        width: 900,
+        height: 600,
+        backgroundColor: '#cce',
+        title: 'FILE-OPEN'
     })
 
     mainWindow.loadURL(url.format({
-        pathname:path.join(__dirname, 'index.html'),
-        protocol:'file:',
-        slashes:true
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
     }))
 
-    mainWindow.once('ready-to-show', ()=>{
+    mainWindow.once('ready-to-show', () => {
         mainWindow.show()
     })
 
-    mainWindow.on('close', function(){
+    mainWindow.on('close', function() {
         mainWindow = null
     })
+
 }
 
-app.on('ready', ()=>{
-    createWindow()
+app.on('ready', createWindow)
+
+//file Open
+
+ipcMain.on('selectFile', function(event) {
+    dialog.showOpenDialog(mainWindow, {
+        title: 'Select a file...',
+        properties: ['openFile'],
+        defaultPath: '/Users/OHM/Documents/',
+        filters: [
+            { name: 'Images', extensions: ['jpg', 'png', 'gif'] },
+            { name: 'Text', extensions: ['txt'] }
+        ]
+    }, function(files) {
+        if (files)
+            event.sender.send('selectedItem', files)
+    })
 })
